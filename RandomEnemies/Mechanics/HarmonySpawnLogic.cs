9bfe72c36cc39d568dd33590252144f0b7ed573e
Snarkki <46639925+Snarkki@users.noreply.github.com>
2021-12-13 05:40:13 +0200

    test

--- RandomEnemies/Mechanics/HarmonySpawnLogic.cs
+++ RandomEnemies/Mechanics/HarmonySpawnLogic.cs
@@ -23,52 +23,71 @@ using Kingmaker.Controllers.Units;
 using Kingmaker.UnitLogic.Groups;
 using static RandomEnemies.Equipment.LootTypes;
 using Kingmaker.Blueprints.JsonSystem;
+using Kingmaker.View.Spawners;
+using Kingmaker.Blueprints.Area;
 
-//namespace RandomEnemies.Mechanics
-//{
-//	public class HarmonySpawnLogic
-//	{
-//		[HarmonyPatch(typeof(BlueprintsCache), "Init")]
-//		private static class ResourcesLibrary_LoadLibrary_Patch
-//		{
-//			private static bool Prefix()
-//			{
-//				Main.Log("ResourcesLibrary_LoadLibrary_Patch Called");
-//				bool initialized = Initialized;
-//				return !initialized;
-//			}
+namespace RandomEnemies.Mechanics
+{
+    internal class HarmonySpawnLogic// : IUnitSpawnHandler, ISubscriber, IGlobalSubscriber
+    {
+        [HarmonyPatch(typeof(Game), "OnAreaLoaded")]
+        internal static class UnitSpawner1_Spawn_Patch
+        {
+            public static void Prefix()
+            {
+                // Set Theme on area load. 
+                SpawnUnitHelper.chosenTheme = SpawnUnitHelper.ChooseTheme();
+                SpawnUnitHelper.backupTheme = SpawnUnitHelper.ChooseBackupTheme(SpawnUnitHelper.chosenTheme);
+            }
+        }
 
-//			private static void Postfix()
-//			{
-//				bool initialized = Initialized;
-//				if (!initialized)
-//				{
-//					Initialized = true;
-//					try
-//					{
-//						RandomSpawnLogic.MainHandler = new RandomSpawnLogic();
-//						RandomSpawnLogic.MainHandler.InitializeProceduralSpawn();
-//					}
-//					catch (Exception ex)
-//					{
-//						Main.LogDebug("Error while patching library");
-//						Main.LogDebug(ex.ToString());
-//					}
-//				}
-//			}
-//			[HarmonyPatch(typeof(Game), "OnAreaLoaded")]
-//			internal static class UnitSpawner1_Spawn_Patch
-//			{
-//				public static void Prefix()
-//				{
-//					// Set Theme on area load. 
-//					SpawnUnitHelper.chosenTheme = SpawnUnitHelper.ChooseTheme();
-//					SpawnUnitHelper.backupTheme = SpawnUnitHelper.ChooseBackupTheme(SpawnUnitHelper.chosenTheme);
-//				}
-//			}
 
-//			private static bool Initialized;
-//		}
-//	}
-//}
+        [HarmonyPatch(typeof(UnitSpawnerBase), "Spawn")]
+        internal static class UnitSpawner2_Spawn_Patch
+        {
 
+            public static void Postfix(UnitSpawnerBase __instance)
+            {
+                UnitEntityData entityData = __instance.Data.SpawnedUnit.Value;
+                if (entityData != null)
+                {
+                    Main.LogDebug(" Loaded area" + Game.Instance.CurrentlyLoadedArea.AreaName + " " + Game.Instance.CurrentlyLoadedArea.AreaName.Key);
+                    bool cutsceneCheck = !Game.Instance.CutsceneLock.Active && !AreaEdits.ProceduralSpawnAreaBlacklist.Any((BlueprintArea x) => x.name == Game.Instance.CurrentlyLoadedArea.name);
+                    if (cutsceneCheck)
+                    {
+                        // Main.LogDebug("Passed CutsCeneCheck");
+                        bool factionCheck = (entityData.Faction == Factions.FactionMobs || entityData.Faction == Factions.FactionWildAnimals);
+                        if (factionCheck)
+                        {
+                            // Main.LogDebug("Passed factionCheck");
+                            bool unitCheck = (!entityData.IsDeadAndHasLoot && !entityData.m_IsDeathRevealed && entityData.MaxHP > 5);
+                            if (unitCheck)
+                            {
+                                //Main.LogDebug("Passed unitCheck");
+                                // blacklist already handled groups & allowed enemy lists & blacklisted unitgroup names (these are added as some are used for cinematic triggers)
+                                if (Units.UnitLists.allowedEnemiesList.Contains(entityData.Blueprint)) // 
+                                {
+                                    // Main.LogDebug("Passed listChecks");
+                                    UnitEntityData result = RandomSpawnLogic.TryCreateEncounter(entityData);
+                                    if (result != null)
+                                    {
+                                        Main.LogDebug(entityData + " CR: " + entityData.CR + " changed blueprint to " + result + " , CR: " + result.CR);
+                                        RandomSpawnLogic.TryCreateLoot(entityData);
+                                    }
+
+                                }
+                                else {
+                                    RandomSpawnLogic.TryCreateLoot(entityData);
+                                    Main.LogDebug(entityData + "Not valid unit blueprint"); 
+                                }
+                            }
+                            else { Main.LogDebug(entityData + "Unit bp was fake/cheater"); }
+                        }
+                        else { Main.LogDebug(entityData + "Faction is not mobs, they are " + __instance.Blueprint.Faction); }
+                    }
+                    else { Main.LogDebug(entityData + "Cutscene active or blacklisted area" + Game.Instance.CurrentlyLoadedArea.AreaName + " " + Game.Instance.CurrentlyLoadedArea.AreaName.Key); }
+                }
+            }
+        }
+    }
+}
